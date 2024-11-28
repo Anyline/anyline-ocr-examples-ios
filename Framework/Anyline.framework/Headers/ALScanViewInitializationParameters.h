19b15c7b0bcd2f5aac670070be365cb25943a7c5
Aldrich Co <aldrich@anyline.com>
2024-11-28 18:42:36 +0100

    54.4.2

--- Framework/Anyline.framework/Headers/ALScanViewInitializationParameters.h
+++ Framework/Anyline.framework/Headers/ALScanViewInitializationParameters.h
@@ -1,6 +1,8 @@
 #import <Foundation/Foundation.h>
 
 @class ALScanViewInitializationParameters;
+@class ALDemo;
+@class ALSfdc;
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -10,7 +12,24 @@ NS_ASSUME_NONNULL_BEGIN
 @interface ALScanViewInitializationParameters : NSObject
 /// An optional uuid (v4) to correlate scans and data points within a workflow.
 @property (nonatomic, nullable, copy) NSString *correlationID;
+/// Data contained within the QR code that's required to unlock scanning with the Showcase
+/// Apps.
+@property (nonatomic, nullable, strong) ALDemo *demo;
 
+@end
+
+/// Data contained within the QR code that's required to unlock scanning with the Showcase
+/// Apps.
+@interface ALDemo : NSObject
+@property (nonatomic, assign)         NSInteger qrVersion;
+@property (nonatomic, strong)         ALSfdc *sfdc;
+@property (nonatomic, nullable, copy) NSString *validityDate;
+@end
+
+@interface ALSfdc : NSObject
+@property (nonatomic, nullable, copy) NSString *accountID;
+@property (nonatomic, nullable, copy) NSString *accountName;
+@property (nonatomic, nullable, copy) NSString *oppID;
 @end
 
 NS_ASSUME_NONNULL_END
